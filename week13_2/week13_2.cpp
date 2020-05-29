2f25df6a56915cdc073998d5418491893852bbc6
桐 孙 <2731881741@qq.com>
2020-05-29 14:40:49 +0800

    练习2

---
+++ week13_2/week13_2.cpp
@@ -0,0 +1,57 @@
+﻿// week13_2.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	VideoCapture capture(0);
+	int cnt = 0;
+	Mat frame;	//存储每一帧的图像
+	Mat tempMat;
+	Mat refMat;
+	Mat resultMat;
+	Mat dispMat;
+	while (1)
+	{
+		capture >> frame;	//读取当前帧
+		if (cnt == 0) {
+			Rect2d r;
+			r = selectROI(frame, true);
+			tempMat = frame(r);
+			tempMat.copyTo(refMat);
+			destroyAllWindows();
+		}
+		int match_method = 0;
+		matchTemplate(frame, refMat, resultMat, match_method);
+		normalize(resultMat, resultMat, 0, 1, NORM_MINMAX, -1, Mat());
+		double minVal; double maxVal; Point minLoc; Point maxLoc; Point matchLoc;
+		minMaxLoc(resultMat, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
+		if (match_method == TM_SQDIFF || match_method == TM_SQDIFF_NORMED)
+			matchLoc = minLoc;
+		else
+			matchLoc = maxLoc;
+		frame.copyTo(dispMat);
+		rectangle(dispMat, matchLoc, Point(matchLoc.x + refMat.cols, matchLoc.y + refMat.rows), Scalar::all(0), 2, 8, 0);
+		cnt++;
+		imshow("template", refMat);
+		imshow("dispMat", dispMat);
+		waitKey(30);
+	}
+	return 0;
+
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
